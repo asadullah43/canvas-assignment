16d8dfdc2b5bd569a3a06ff1e4f6daa0e05d5082
asadullah <asad.rehman@devsinc.com>
2022-03-16 20:11:20 +0500

    spacing issue

--- script.js
+++ script.js
@@ -31,19 +31,20 @@ imgInput.addEventListener("change", function (e) {
     };
   }
 });
+
 function addHeader() {
   var header_text = document.getElementById("header-input").value;
   myContext.font = "15px Arial";
   myContext.fillStyle = "black";
   myContext.fillText(header_text, myCanvas.width / 5, 12);
 }
+
 function addFooter() {
   var footer_text = document.getElementById("footer-input").value;
   myContext.font = "15px Arial";
   myContext.fillStyle = "black";
   myContext.fillText(footer_text, myCanvas.width / 8, myCanvas.height - 4);
 }
-
 document.getElementById("text-btn").onclick = function () {
   var my_text = document.getElementById("canvas-text").value;
   console.log(my_text);
@@ -51,8 +52,9 @@ document.getElementById("text-btn").onclick = function () {
   myContext.fillStyle = "red";
   myContext.fillText(my_text, myCanvas.width / 2, myCanvas.height / 2);
 };
+
 function download() {
-  var download = document.getElementById("download");
+  var download = document.getElementById("imageCanvas");
   var image = document
     .getElementById("myCanvas")
     .toDataURL("image/png")
